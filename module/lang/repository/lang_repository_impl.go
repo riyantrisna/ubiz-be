fb20c3b11f6928e83fe2248e9ccbb9e07070c600
Riyan Trisna Wibowo <riyantrisnawibowo@gmail.com>
2022-08-28 23:03:49 +0700

    rapihkan query user and lang

--- module/lang/repository/lang_repository_impl.go
+++ module/lang/repository/lang_repository_impl.go
@@ -16,8 +16,23 @@ func NewLangRepository() LangRepository {
 
 func (repository *LangRepositoryImpl) Save(ctx context.Context, tx *sql.Tx, lang domain.Lang) domain.Lang {
 
-	SQL := "INSERT INTO lang(lang_code, lang_name, created_by, created_at) VALUES (?, ?, ?, ?)"
-	result, err := tx.ExecContext(ctx, SQL, lang.LangCode, lang.LangName, lang.CreatedBy, lang.CreatedAt)
+	SQL := `INSERT INTO lang
+			(
+				lang_code, 
+				lang_name, 
+				created_by, 
+				created_at
+			) VALUES (
+				?, 
+				?, 
+				?, 
+				?
+			)`
+	result, err := tx.ExecContext(ctx, SQL,
+		lang.LangCode,
+		lang.LangName,
+		lang.CreatedBy,
+		lang.CreatedAt)
 	helper.PanicIfError(err)
 
 	id, err := result.LastInsertId()
@@ -28,28 +43,59 @@ func (repository *LangRepositoryImpl) Save(ctx context.Context, tx *sql.Tx, lang
 }
 
 func (repository *LangRepositoryImpl) Update(ctx context.Context, tx *sql.Tx, lang domain.Lang) domain.Lang {
-	SQL := "UPDATE lang SET lang_code = ?, lang_name = ?, updated_by = ?, updated_at = ? WHERE lang_id = ?"
-	_, err := tx.ExecContext(ctx, SQL, lang.LangCode, lang.LangName, lang.UpdatedBy, lang.UpdatedAt, lang.LangId)
+	SQL := `UPDATE 
+				lang 
+			SET 
+				lang_code = ?, 
+				lang_name = ?, 
+				updated_by = ?, 
+				updated_at = ? 
+			WHERE 
+				lang_id = ?`
+	_, err := tx.ExecContext(ctx, SQL,
+		lang.LangCode,
+		lang.LangName,
+		lang.UpdatedBy,
+		lang.UpdatedAt,
+		lang.LangId)
 	helper.PanicIfError(err)
 
 	return lang
 }
 
 func (repository *LangRepositoryImpl) Delete(ctx context.Context, tx *sql.Tx, lang domain.Lang) {
-	SQL := "DELETE FROM lang WHERE lang_id = ?"
+	SQL := `DELETE FROM lang WHERE lang_id = ?`
 	_, err := tx.ExecContext(ctx, SQL, lang.LangId)
 	helper.PanicIfError(err)
 }
 
 func (repository *LangRepositoryImpl) FindById(ctx context.Context, tx *sql.Tx, langId int) (domain.Lang, error) {
-	SQL := "SELECT lang_id, lang_code, lang_name, IFNULL(created_by,0), IFNULL(created_at,''), IFNULL(updated_by,0), IFNULL(updated_at,'') FROM lang WHERE lang_id = ?"
+	SQL := `SELECT 
+				lang_id, 
+				lang_code, 
+				lang_name, 
+				IFNULL(created_by,0), 
+				IFNULL(created_at,''), 
+				IFNULL(updated_by,0), 
+				IFNULL(updated_at,'') 
+			FROM 
+				lang 
+			WHERE 
+				lang_id = ?`
 	rows, err := tx.QueryContext(ctx, SQL, langId)
 	helper.PanicIfError(err)
 	defer rows.Close()
 
 	lang := domain.Lang{}
 	if rows.Next() {
-		err := rows.Scan(&lang.LangId, &lang.LangCode, &lang.LangName, &lang.CreatedBy, &lang.CreatedAt, &lang.UpdatedBy, &lang.UpdatedAt)
+		err := rows.Scan(
+			&lang.LangId,
+			&lang.LangCode,
+			&lang.LangName,
+			&lang.CreatedBy,
+			&lang.CreatedAt,
+			&lang.UpdatedBy,
+			&lang.UpdatedAt)
 		helper.PanicIfError(err)
 	}
 
@@ -57,7 +103,16 @@ func (repository *LangRepositoryImpl) FindById(ctx context.Context, tx *sql.Tx, 
 }
 
 func (repository *LangRepositoryImpl) FindAll(ctx context.Context, tx *sql.Tx) []domain.Lang {
-	SQL := "SELECT lang_id, lang_code, lang_name, IFNULL(created_by,0), IFNULL(created_at,''), IFNULL(updated_by,0), IFNULL(updated_at,'') FROM lang"
+	SQL := `SELECT 
+				lang_id, 
+				lang_code, 
+				lang_name, 
+				IFNULL(created_by,0), 
+				IFNULL(created_at,''), 
+				IFNULL(updated_by,0), 
+				IFNULL(updated_at,'') 
+			FROM 
+				lang`
 	rows, err := tx.QueryContext(ctx, SQL)
 	helper.PanicIfError(err)
 	defer rows.Close()
@@ -65,7 +120,14 @@ func (repository *LangRepositoryImpl) FindAll(ctx context.Context, tx *sql.Tx) [
 	var langs []domain.Lang
 	for rows.Next() {
 		lang := domain.Lang{}
-		err := rows.Scan(&lang.LangId, &lang.LangCode, &lang.LangName, &lang.CreatedBy, &lang.CreatedAt, &lang.UpdatedBy, &lang.UpdatedAt)
+		err := rows.Scan(
+			&lang.LangId,
+			&lang.LangCode,
+			&lang.LangName,
+			&lang.CreatedBy,
+			&lang.CreatedAt,
+			&lang.UpdatedBy,
+			&lang.UpdatedAt)
 		helper.PanicIfError(err)
 		langs = append(langs, lang)
 	}
--- module/user/repository/user_repository_impl.go
+++ module/user/repository/user_repository_impl.go
@@ -16,8 +16,29 @@ func NewUserRepository() UserRepository {
 
 func (repository *UserRepositoryImpl) Save(ctx context.Context, tx *sql.Tx, user domain.User) domain.User {
 
-	SQL := "INSERT INTO user(user_name, user_email, user_password, user_lang_code, created_by, created_at) VALUES (?, ?, ?, ?, ?, ?)"
-	result, err := tx.ExecContext(ctx, SQL, user.UserName, user.UserEmail, user.UserPassword, user.UserLangCode, user.CreatedBy, user.CreatedAt)
+	SQL := `INSERT INTO user
+			(
+				user_name, 
+				user_email, 
+				user_password, 
+				user_lang_code, 
+				created_by, 
+				created_at
+			) VALUES (
+				?, 
+				?, 
+				?, 
+				?, 
+				?, 
+				?
+			)`
+	result, err := tx.ExecContext(ctx, SQL,
+		user.UserName,
+		user.UserEmail,
+		user.UserPassword,
+		user.UserLangCode,
+		user.CreatedBy,
+		user.CreatedAt)
 	helper.PanicIfError(err)
 
 	id, err := result.LastInsertId()
@@ -28,34 +49,88 @@ func (repository *UserRepositoryImpl) Save(ctx context.Context, tx *sql.Tx, user
 }
 
 func (repository *UserRepositoryImpl) Update(ctx context.Context, tx *sql.Tx, user domain.User) domain.User {
-	SQL := "UPDATE user SET user_name = ?, user_email = ?, user_lang_code = ?, updated_by = ?, updated_at = ? WHERE user_id = ?"
-	_, err := tx.ExecContext(ctx, SQL, user.UserName, user.UserEmail, user.UserLangCode, user.UpdatedBy, user.UpdatedAt, user.UserId)
+	SQL := `UPDATE 
+				user 
+			SET 
+				user_name = ?, 
+				user_email = ?, 
+				user_lang_code = ?, 
+				updated_by = ?, 
+				updated_at = ? 
+			WHERE 
+				user_id = ?`
+	_, err := tx.ExecContext(ctx, SQL,
+		user.UserName,
+		user.UserEmail,
+		user.UserLangCode,
+		user.UpdatedBy,
+		user.UpdatedAt,
+		user.UserId)
 	helper.PanicIfError(err)
 
 	return user
 }
 
 func (repository *UserRepositoryImpl) Delete(ctx context.Context, tx *sql.Tx, user domain.User) {
-	SQL := "DELETE FROM user WHERE user_id = ?"
+	SQL := `DELETE FROM user WHERE user_id = ?`
 	_, err := tx.ExecContext(ctx, SQL, user.UserId)
 	helper.PanicIfError(err)
 }
 
 func (repository *UserRepositoryImpl) SoftDelete(ctx context.Context, tx *sql.Tx, user domain.User) {
-	SQL := "UPDATE user SET deleted_by = ?, deleted_at = ? WHERE user_id = ?"
-	_, err := tx.ExecContext(ctx, SQL, user.DeletedBy, user.DeletedAt, user.UserId)
+	SQL := `UPDATE 
+				user 
+			SET 
+				deleted_by = ?, 
+				deleted_at = ? 
+			WHERE 
+				user_id = ?`
+	_, err := tx.ExecContext(ctx, SQL,
+		user.DeletedBy,
+		user.DeletedAt,
+		user.UserId)
 	helper.PanicIfError(err)
 }
 
 func (repository *UserRepositoryImpl) FindById(ctx context.Context, tx *sql.Tx, userId int) (domain.User, error) {
-	SQL := "SELECT user_id, user_name, user_email, IFNULL(user_token,''), IFNULL(user_token_refresh,''), user_lang_code, IFNULL(user_last_login,''), IFNULL(created_by,0), IFNULL(created_at,''), IFNULL(updated_by,0), IFNULL(updated_at,''), IFNULL(deleted_by,0), IFNULL(deleted_at,'') FROM user WHERE user_id = ?"
+	SQL := `SELECT 
+				user_id, 
+				user_name, 
+				user_email, 
+				IFNULL(user_token,''), 
+				IFNULL(user_token_refresh,''), 
+				user_lang_code, 
+				IFNULL(user_last_login,''), 
+				IFNULL(created_by,0), 
+				IFNULL(created_at,''), 
+				IFNULL(updated_by,0), 
+				IFNULL(updated_at,''), 
+				IFNULL(deleted_by,0), 
+				IFNULL(deleted_at,'') 
+			FROM 
+				user 
+			WHERE 
+				user_id = ?`
 	rows, err := tx.QueryContext(ctx, SQL, userId)
 	helper.PanicIfError(err)
 	defer rows.Close()
 
 	user := domain.User{}
 	if rows.Next() {
-		err := rows.Scan(&user.UserId, &user.UserName, &user.UserEmail, &user.UserToken, &user.UserTokenRefresh, &user.UserLangCode, &user.UserLastLogin, &user.CreatedBy, &user.CreatedAt, &user.UpdatedBy, &user.UpdatedAt, &user.DeletedBy, &user.DeletedAt)
+		err := rows.Scan(
+			&user.UserId,
+			&user.UserName,
+			&user.UserEmail,
+			&user.UserToken,
+			&user.UserTokenRefresh,
+			&user.UserLangCode,
+			&user.UserLastLogin,
+			&user.CreatedBy,
+			&user.CreatedAt,
+			&user.UpdatedBy,
+			&user.UpdatedAt,
+			&user.DeletedBy,
+			&user.DeletedAt)
 		helper.PanicIfError(err)
 	}
 
@@ -63,7 +138,22 @@ func (repository *UserRepositoryImpl) FindById(ctx context.Context, tx *sql.Tx, 
 }
 
 func (repository *UserRepositoryImpl) FindAll(ctx context.Context, tx *sql.Tx) []domain.User {
-	SQL := "SELECT user_id, user_name, user_email, IFNULL(user_token,''), IFNULL(user_token_refresh,''), user_lang_code, IFNULL(user_last_login,''), IFNULL(created_by,0), IFNULL(created_at,''), IFNULL(updated_by,0), IFNULL(updated_at,''), IFNULL(deleted_by,0), IFNULL(deleted_at,'') FROM user"
+	SQL := `SELECT 
+				user_id, 
+				user_name, 
+				user_email, 
+				IFNULL(user_token,''), 
+				IFNULL(user_token_refresh,''), 
+				user_lang_code, 
+				IFNULL(user_last_login,''), 
+				IFNULL(created_by,0), 
+				IFNULL(created_at,''), 
+				IFNULL(updated_by,0), 
+				IFNULL(updated_at,''), 
+				IFNULL(deleted_by,0), 
+				IFNULL(deleted_at,'') 
+			FROM 
+				user`
 	rows, err := tx.QueryContext(ctx, SQL)
 	helper.PanicIfError(err)
 	defer rows.Close()
@@ -71,7 +161,20 @@ func (repository *UserRepositoryImpl) FindAll(ctx context.Context, tx *sql.Tx) [
 	var users []domain.User
 	for rows.Next() {
 		user := domain.User{}
-		err := rows.Scan(&user.UserId, &user.UserName, &user.UserEmail, &user.UserToken, &user.UserTokenRefresh, &user.UserLangCode, &user.UserLastLogin, &user.CreatedBy, &user.CreatedAt, &user.UpdatedBy, &user.UpdatedAt, &user.DeletedBy, &user.DeletedAt)
+		err := rows.Scan(
+			&user.UserId,
+			&user.UserName,
+			&user.UserEmail,
+			&user.UserToken,
+			&user.UserTokenRefresh,
+			&user.UserLangCode,
+			&user.UserLastLogin,
+			&user.CreatedBy,
+			&user.CreatedAt,
+			&user.UpdatedBy,
+			&user.UpdatedAt,
+			&user.DeletedBy,
+			&user.DeletedAt)
 		helper.PanicIfError(err)
 		users = append(users, user)
 	}
@@ -80,14 +183,26 @@ func (repository *UserRepositoryImpl) FindAll(ctx context.Context, tx *sql.Tx) [
 }
 
 func (repository *UserRepositoryImpl) FindByEmail(ctx context.Context, tx *sql.Tx, userEmail string) (domain.User, error) {
-	SQL := "SELECT user_id, user_name, user_password, user_lang_code FROM user WHERE user_email = ?"
+	SQL := `SELECT 
+				user_id, 
+				user_name, 
+				user_password, 
+				user_lang_code 
+			FROM 
+				user 
+			WHERE 
+				user_email = ?`
 	rows, err := tx.QueryContext(ctx, SQL, userEmail)
 	helper.PanicIfError(err)
 	defer rows.Close()
 
 	user := domain.User{}
 	if rows.Next() {
-		err := rows.Scan(&user.UserId, &user.UserName, &user.UserPassword, &user.UserLangCode)
+		err := rows.Scan(
+			&user.UserId,
+			&user.UserName,
+			&user.UserPassword,
+			&user.UserLangCode)
 		helper.PanicIfError(err)
 	}
 
@@ -95,14 +210,26 @@ func (repository *UserRepositoryImpl) FindByEmail(ctx context.Context, tx *sql.T
 }
 
 func (repository *UserRepositoryImpl) FindByTokenRefresh(ctx context.Context, tx *sql.Tx, userTokenRefresh string) (domain.User, error) {
-	SQL := "SELECT user_id, user_name, user_password, user_lang_code FROM user WHERE user_token_refresh = ?"
+	SQL := `SELECT 
+				user_id, 
+				user_name, 
+				user_password, 
+				user_lang_code 
+			FROM 
+				user 
+			WHERE 
+				user_token_refresh = ?`
 	rows, err := tx.QueryContext(ctx, SQL, userTokenRefresh)
 	helper.PanicIfError(err)
 	defer rows.Close()
 
 	user := domain.User{}
 	if rows.Next() {
-		err := rows.Scan(&user.UserId, &user.UserName, &user.UserPassword, &user.UserLangCode)
+		err := rows.Scan(
+			&user.UserId,
+			&user.UserName,
+			&user.UserPassword,
+			&user.UserLangCode)
 		helper.PanicIfError(err)
 	}
 
@@ -110,15 +237,32 @@ func (repository *UserRepositoryImpl) FindByTokenRefresh(ctx context.Context, tx
 }
 
 func (repository *UserRepositoryImpl) UpdateToken(ctx context.Context, tx *sql.Tx, user domain.User) domain.User {
-	SQL := "UPDATE user SET user_token = ?, user_token_refresh = ?, user_last_login = ? WHERE user_id = ?"
-	_, err := tx.ExecContext(ctx, SQL, user.UserToken, user.UserTokenRefresh, user.UserLastLogin, user.UserId)
+	SQL := `UPDATE 
+				user 
+			SET 
+				user_token = ?, 
+				user_token_refresh = ?, 
+				user_last_login = ? 
+			WHERE 
+				user_id = ?`
+	_, err := tx.ExecContext(ctx, SQL,
+		user.UserToken,
+		user.UserTokenRefresh,
+		user.UserLastLogin,
+		user.UserId)
 	helper.PanicIfError(err)
 
 	return user
 }
 
 func (repository *UserRepositoryImpl) Logout(ctx context.Context, tx *sql.Tx, user domain.User) domain.User {
-	SQL := "UPDATE user SET user_token = NULL, user_token_refresh = NULL WHERE user_id = ?"
+	SQL := `UPDATE 
+				user 
+			SET 
+				user_token = NULL, 
+				user_token_refresh = NULL 
+			WHERE 
+				user_id = ?`
 	_, err := tx.ExecContext(ctx, SQL, user.UserId)
 	helper.PanicIfError(err)
 
