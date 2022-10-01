0f4806b3150abdf4f49e9bd022a49d8fb7844f72
Gavinda Kinandana <gavindakinandana@gmail.com>
2022-10-01 20:59:11 +0700

    change max idle and max lifetime config to time.duration

--- configs/config.go
+++ configs/config.go
@@ -14,15 +14,15 @@ type Config struct {
 	Address  string `mapstructure:"ADDRESS"`
 	Database struct {
 		Mysql struct {
-			DbHost          string `mapstructure:"DBHOST"`
-			DbPort          int    `mapstructure:"DBPORT"`
-			DbUser          string `mapstructure:"DBUSER"`
-			DbPass          string `mapstructure:"DBPASS"`
-			DbName          string `mapstructure:"DBNAME"`
-			MaxIdleConns    int    `mapstructure:"MAXIDLECONNS"`
-			MaxOpenConns    int    `mapstructure:"MAXOPENCONNS"`
-			ConnMaxLifeTime int    `mapstructure:"CONNMAXLIFETIME"`
-			ConnMaxIdleTime int    `mapstructure:"CONNMAXIDLETIME"`
+			DbHost          string        `mapstructure:"DBHOST"`
+			DbPort          int           `mapstructure:"DBPORT"`
+			DbUser          string        `mapstructure:"DBUSER"`
+			DbPass          string        `mapstructure:"DBPASS"`
+			DbName          string        `mapstructure:"DBNAME"`
+			MaxIdleConns    int           `mapstructure:"MAXIDLECONNS"`
+			MaxOpenConns    int           `mapstructure:"MAXOPENCONNS"`
+			ConnMaxLifeTime time.Duration `mapstructure:"CONNMAXLIFETIME"`
+			ConnMaxIdleTime time.Duration `mapstructure:"CONNMAXIDLETIME"`
 		} `mapstructure:"MYSQL"`
 	} `mapstructure:"DATABASE"`
 	JWT struct {
